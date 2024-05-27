235656fa51d8aa141c02a04e3fbc96efe2effb61
guisaez <guillesaez15@icloud.com>
2024-05-27 00:04:17 -0400

    Part 1

---
+++ p2p/tcp_transport.go
@@ -0,0 +1,94 @@
+package p2p
+
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+// TCP represents the remote node over TCP established connection
+type TCPPeer struct {
+	// conn is underlying connection fo the peer
+	conn net.Conn
+	// if we dial and retrieve a conn => outbound == true
+	// if we accept and retrieve a conn => outbound == false
+	outbound bool
+}
+
+func NewTCPPeer(conn net.Conn, outbound bool) *TCPPeer {
+	return &TCPPeer{
+		conn:     conn,
+		outbound: outbound,
+	}
+}
+
+type TCPTransportOpts struct {
+	ListenAddr    string
+	HandshakeFunc HandshakeFunc
+	Decoder       Decoder
+}
+
+type TCPTransport struct {
+	TCPTransportOpts
+	listener net.Listener
+
+	mu    *sync.RWMutex
+	peers map[net.Addr]Peer
+}
+
+func NewTCPTransport(opts TCPTransportOpts) *TCPTransport {
+	return &TCPTransport{
+		TCPTransportOpts: opts,
+	}
+}
+
+func (t *TCPTransport) ListenAndAccept() error {
+
+	var err error
+	t.listener, err = net.Listen("tcp", t.ListenAddr)
+	if err != nil {
+		return err
+	}
+
+	go t.startAcceptLoop()
+
+	return nil
+}
+
+func (t *TCPTransport) startAcceptLoop() {
+	for {
+		conn, err := t.listener.Accept()
+		if err != nil {
+			fmt.Printf("tcp accept error: %s\n", err)
+		}
+
+		fmt.Printf("tcp: new incoming connection %+v\n", conn)
+
+		go t.handleConn(conn)
+	}
+}
+
+type Temp struct{}
+
+func (t *TCPTransport) handleConn(conn net.Conn) {
+	peer := NewTCPPeer(conn, true)
+
+	if err := t.HandshakeFunc(peer); err != nil {
+		conn.Close()
+		fmt.Printf("tcp handshake error: %s\n", err)
+		return
+	}
+
+	// Read Loop
+	msg := &Message{}
+	for {
+		if err := t.Decoder.Decode(conn, msg); err != nil {
+			fmt.Printf("tcp error: %s\n", err)
+			continue
+		}
+
+		msg.From = conn.RemoteAddr()
+		
+		fmt.Printf("message: %+v\n", msg)
+	}
+}
